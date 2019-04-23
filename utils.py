f33b3f3f8140b60fd4fde9c6abdf79f61d483fff
ivan <ivan.pobeguc@gmail.com>
2019-04-23 18:34:53 +0300

    (*) minimize spaceship borders, add dockstrings

--- utils.py
+++ utils.py
@@ -1,11 +1,14 @@
 import os
 
+
 def read_rocket_frames(directory):
-  frames = []
+    """Read frames from specified directory"""
 
-  for file in os.listdir(f"{directory}"):
-      with open(f"{directory}/{file}", "r") as my_file:
-          frame = my_file.read()
-          frames.append(frame)
+    frames = []
 
-  return frames
+    for file in os.listdir(f"{directory}"):
+        with open(f"{directory}/{file}", "r") as my_file:
+            frame = my_file.read()
+            frames.append(frame)
+
+    return frames
