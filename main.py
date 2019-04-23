d3de505c828972fcc35e3e626e932e3463266e0c
ivan <ivan.pobeguc@gmail.com>
2019-04-23 18:35:10 +0300

    (*) minimize spaceship borders, add dockstrings

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ from utils import read_rocket_frames
 from curses_tools import *
 
 ROCKET_FRAMES = read_rocket_frames('graphics')
+BORDER_WIDTH = 1
 
 
 async def blink(canvas, row, column, symbol='*'):
+    """Change 'star' intensity"""
+
     delay = random.randint(0, 10)
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
@@ -31,12 +34,20 @@ async def blink(canvas, row, column, symbol='*'):
             await asyncio.sleep(0)
 
 
-async def animate_spaceship(canvas, row, column):
+async def animate_spaceship(canvas, row, column, max_row, max_col):
+    """Animate spaceship according to the user input"""
+
+    row_size, col_size = get_frame_size(ROCKET_FRAMES[0])
+
     while True:
-        rows_direction, columns_direction, _ = read_controls(canvas)
-        row = row + rows_direction
-        column = column + 2*columns_direction
         for frame in ROCKET_FRAMES:
+            rows_direction, columns_direction, _ = read_controls(canvas)
+            new_row = row + rows_direction
+            new_column = column + 2 * columns_direction
+            if BORDER_WIDTH <= new_row <= max_row - row_size - BORDER_WIDTH:
+                row = new_row
+            if BORDER_WIDTH <= new_column <= max_col - col_size + 1 - BORDER_WIDTH:
+                column = new_column
             draw_frame(canvas, row, column, frame)
             await asyncio.sleep(0)
             draw_frame(canvas, row, column, frame, negative=True)
@@ -73,19 +84,22 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0
 
 
 def draw(canvas):
-    # row, column = (5, 20)
+    """Start main loop for coroutines and draw all graphics"""
+
     TIC_TIMEOUT = 0.1
     coroutines = []
     max_row, max_col = canvas.getmaxyx()
     limit = random.randint(10, 100)
     canvas.nodelay(True)
+
     coroutines.append(fire(canvas, max_row / 2, max_col / 2))
-    coroutines.append(animate_spaceship(canvas, max_row / 2, max_col / 2 - 2))
+    coroutines.append(animate_spaceship(canvas, max_row / 2, max_col / 2 - 2, max_row, max_col))
     for i in range(limit):
         column = random.randint(1, max_col - 1)
         row = random.randint(1, max_row - 1)
         symbol = random.choice('+*.:')
         coroutines.append(blink(canvas, row, column, symbol))
+
     while True:
         curses.curs_set(False)
         canvas.border()
